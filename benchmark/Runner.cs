37250b7e679d335673769a61bd49cc8ff0c1d51c
Shoichi Yasui <syasui@orange-cube.jp>
2023-02-28 00:53:52 +0900

    fix: call DisposeAsync

--- benchmark/Runner.cs
+++ benchmark/Runner.cs
@@ -31,16 +31,36 @@ public class Runner
     }
 
     [Benchmark]
-    public Task Delay() => Benchmark(new benchmark.Delay.SequentialSynchronizationContext(TimeSpan.FromMilliseconds(100), OnError));
+    public async Task Delay()
+    {
+        var s = new benchmark.Delay.SequentialSynchronizationContext(TimeSpan.FromMilliseconds(100), OnError);
+        await Benchmark(s);
+        await s.DisposeAsync();
+    }
 
     [Benchmark]
-    public Task DelayWithLock() => Benchmark(new benchmark.DelayLock.SequentialSynchronizationContext(TimeSpan.FromMilliseconds(100), OnError));
+    public async Task DelayWithLock()
+    {
+        var s = new benchmark.DelayLock.SequentialSynchronizationContext(TimeSpan.FromMilliseconds(100), OnError);
+        await Benchmark(s);
+        await s.DisposeAsync();
+    }
 
     [Benchmark]
-    public Task Semaphore() => Benchmark(new benchmark.Semaphore.SequentialSynchronizationContext(OnError));
+    public async Task Semaphore()
+    {
+        var s = new benchmark.Semaphore.SequentialSynchronizationContext(OnError);
+        await Benchmark(s);
+        await s.DisposeAsync();
+    }
 
     [Benchmark]
-    public Task Channel() => Benchmark(new benchmark.Channels.SequentialSynchronizationContext(OnError));
+    public async Task Channel()
+    {
+        var s = new benchmark.Channels.SequentialSynchronizationContext(OnError);
+        await Benchmark(s);
+        await s.DisposeAsync();
+    }
 
 
     private async Task Benchmark(SynchronizationContext synchronizationContext)

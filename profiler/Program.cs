a378253cc4c1fcfaa21194085b04361d6465aa5a
Shoichi Yasui <syasui@orange-cube.jp>
2023-02-28 08:20:47 +0900

    chore: add profiler

---
+++ profiler/Program.cs
@@ -0,0 +1,7 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnostics.Windows;
+using BenchmarkDotNet.Running;
+
+BenchmarkSwitcher
+    .FromAssembly(typeof(Program).Assembly)
+    .Run(args, DefaultConfig.Instance.AddDiagnoser(new EtwProfiler()));
---
+++ profiler/Runner.cs
@@ -0,0 +1,48 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+using System.Runtime.ExceptionServices;
+
+[EtwProfiler]
+public class Runner
+{
+    [Benchmark(Baseline = true)]
+    public async Task Delay()
+    {
+        var s = new benchmark.Delay.SequentialSynchronizationContext(TimeSpan.FromMilliseconds(100), OnError);
+        await Benchmark(s);
+        await s.DisposeAsync();
+    }
+
+    [Benchmark]
+    public async Task DelayWithLock()
+    {
+        var s = new benchmark.DelayLock.SequentialSynchronizationContext(TimeSpan.FromMilliseconds(100), OnError);
+        await Benchmark(s);
+        await s.DisposeAsync();
+    }
+
+    [Benchmark]
+    public async Task Semaphore()
+    {
+        var s = new benchmark.Semaphore.SequentialSynchronizationContext(OnError);
+        await Benchmark(s);
+        await s.DisposeAsync();
+    }
+
+    [Benchmark]
+    public async Task Channel()
+    {
+        var s = new benchmark.Channels.SequentialSynchronizationContext(OnError);
+        await Benchmark(s);
+        await s.DisposeAsync();
+    }
+
+
+    private async Task Benchmark(SynchronizationContext synchronizationContext)
+    {
+        await Task.Delay(TimeSpan.FromSeconds(10));
+    }
+
+    private static void OnError(Exception ex)
+        => ExceptionDispatchInfo.Capture(ex).Throw();
+}
